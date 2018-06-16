f549c6002c552dd98c12763285ca5576948d6dba
Sergey Gorobets <gorserg@hotmail.com>
2018-06-16 13:21:21 +0300

    Add react libs

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,25 @@
+const News = () => {
+    const date = new Date().toISOString();
+    return (
+     <div className="news">{'Empty news for date' +  date}
+    </div>);
+};
+
+const Comments = () => (
+    <div className="comments">
+        Empty comments
+    </div>
+);
+
+const App = () => (
+    <>
+        App title
+        <News/>
+        <Comments/>
+    </>
+);
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <App/>,
     document.getElementById('root')
 );
