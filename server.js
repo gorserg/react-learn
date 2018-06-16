ee3f0a8fb73f2c3154e1dae530ca1a432f915c40
Sergey Gorobets <gorserg@hotmail.com>
2018-06-16 11:42:31 +0300

    Add version route

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(__dirname));
+app.use('/', express.static('public'));
+
+app.get('/api/version', function (req, res) {
+    const packageInfo = require('./package');
+    res.json({ "version" : packageInfo.version, "author" : packageInfo.author});
+});
 
 app.get('/api/ping', function (req, res) {
     res.json({ "ping": new Date().toISOString(), "param" : req.query.id });
