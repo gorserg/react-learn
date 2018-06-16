6e21e2c918d832407d4bb79873e054a6c6955153
Sergey Gorobets <gorserg@hotmail.com>
2018-06-16 14:44:34 +0300

    add props

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,34 @@
-const News = () => {
-    const date = new Date().toISOString();
+const newsArray = [
+    {
+        author: 'Author 1',
+        text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+    {
+        author: 'Author 2',
+        text: ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    },
+    {
+        author: 'Author 3',
+        text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+];
+
+
+const News = (props) => {
+    const {data} = props;
+    var newsTemplate = data.map(function (item, index) {
+        return (
+            <div key={index}>
+                <p className="news__author">{item.author}:</p>
+                <p className="news__text">{item.text}</p>
+            </div>
+        )
+    });
     return (
-     <div className="news">{'Empty news for date' +  date}
-    </div>);
+        <div className="news">
+            {newsTemplate}
+        </div>
+    );
 };
 
 function CommentsAsFunction(){
@@ -33,10 +59,7 @@ class CommentsAsClass extends React.Component {
 const App = () => (
     <div>
         App title
-        <News/>
-        <CommentsAsFunction/>
-        <CommentsAsArrayFunc/>
-        <CommentsAsClass/>
+        <News data={newsArray} />
     </div>
 );
 
