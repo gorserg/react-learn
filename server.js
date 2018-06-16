9739dc3fff7dc7b2089028861424f0e1e73a9f74
Sergey Gorobets <gorserg@hotmail.com>
2018-06-16 11:31:20 +0300

    Add data.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(__dirname));
 
 app.get('/api/ping', function (req, res) {
-    res.json({ "ping": new Date().toISOString() });
+    res.json({ "ping": new Date().toISOString(), "param" : req.query.id });
+});
+
+app.get('/api/data', function (req, res) {
+    res.json(require("./data/fakeData"));
 });
 
 app.listen(app.get('port'), function() {
